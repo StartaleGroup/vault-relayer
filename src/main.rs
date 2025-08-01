0992e3eff44d7ac130f1663922acd49584b4c20d
web3nish <const.nishant@gmail.com>
2025-08-01 17:36:49 +0400

    Updating main rs

--- src/main.rs
+++ src/main.rs
@@ -22,11 +22,9 @@ async fn main() -> anyhow::Result<()> {
 
     let user: Address = user_str.parse()?;
     let token: Address = token_str.parse()?;
-    let vault_address_addr: Address = vault_address.parse()?;
 
-    let amount = U256::from_str("100000000000000")?; // 0.0001 ETH
-    let nonce = U256::from(1); // Replace with actual nonce tracking later
-
+    let amount = U256::from_str("100000000000000")?; // 0.001 ETH
+    let nonce = U256::from(4); // Replace with actual nonce tracking later
     let vault = VaultClient::new(&rpc_url, &vault_address).await?;
 
     // Step 1: Get balance on OP/Base
@@ -42,10 +40,14 @@ async fn main() -> anyhow::Result<()> {
 
     // Step 2: Lock on OP/Base
     println!("✍️ Signing redeem intent for Soneium...");
+    let op_rpc_url = env::var("OP_RPC_URL")?;
+    let op_vault_address = env::var("OP_VAULT_CONTRACT")?;
+    let op_vault_addr: Address = op_vault_address.parse()?;
+    let op_vault = VaultClient::new(&op_rpc_url, &op_vault_address).await?;
     let soneium_rpc_url = env::var("SONEIUM_RPC_URL")?;
     let soneium_vault_address = env::var("SONEIUM_VAULT_CONTRACT")?;
     let soneium_vault_addr: Address = soneium_vault_address.parse()?;
-    let soneium_vault = VaultClient::new(&soneium_rpc_url, &soneium_vault_address).await?;
+
 
     let (digest, signature) = sign_redeem_hash(
         &private_key,
@@ -55,24 +57,31 @@ async fn main() -> anyhow::Result<()> {
         nonce,
         soneium_vault_addr,
     )?;
+
+    println!("🔐 Digest: {digest:?}");
+    println!("🖋️ Signature for Soneium: 0x{}", hex::encode(&signature));
+
+    // For destination chain for claims
+    let (digest, signature) = sign_redeem_hash(
+        &private_key,
+        user,
+        token,
+        amount,
+        nonce,
+        op_vault_addr,
+    )?;
+
+    println!("🔐 Digest: {digest:?}");
+    println!("🖋️ Signature for OP: 0x{}", hex::encode(&signature));
+
     println!("🔐 Locking funds on OP/Base vault...");
     vault
         .lock(user, token, amount, nonce, signature.clone().into())
         .await?;
     println!("✅ Funds locked on OP/Base.");
 
-
-    // Step 3: Sign Fill intent for Soneium
-
-
-    println!("🔐 Digest: {digest:?}");
-    println!("🖋️ Signature: 0x{}", hex::encode(&signature));
-
-    println!("🌉 Submitting redeem to Soneium...");
-    let tx_hash = soneium_vault
-        .redeem_with_signature(user, token, amount, nonce, signature.clone().into())
-        .await?;
-    println!("✅ Soneium redeem tx hash: {tx_hash:?}");
+    // Step 3: Sign Fill intent for Soneium => This happens trueless via layer-zero 
+    // ----------------------------------------------------------------------------
 
     // Step 4: Claim the intent =>laiming from OP vault => This will happen (after redemption proof confirmed) - LockedFunds
     let (digest_op, signature_op) = sign_redeem_hash(
@@ -81,7 +90,7 @@ async fn main() -> anyhow::Result<()> {
         token,
         amount,
         nonce,
-        vault_address_addr,
+        op_vault_addr,
     )?;
 
     println!("🔐 Digest: {digest_op:?}");
@@ -91,8 +100,6 @@ async fn main() -> anyhow::Result<()> {
         .redeem_with_signature(user, token, amount, nonce, signature_op.into())
         .await?;
     println!("✅ OP/Base claim tx: {tx_hash_op:?}");
-
-
 
     Ok(())
 }
